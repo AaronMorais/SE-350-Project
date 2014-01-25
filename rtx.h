53fba52578cdfe766627779f5d3bbbab75b8ef1e
crazy2be <code@jmcgirr.com>
2014-01-25 13:46:27 -0500

    Cleanup syscall implementation a bit.

--- rtx.h
+++ rtx.h
@@ -4,21 +4,7 @@
 
 #include "rtx_shared.h"
 
-#define __SVC_0  __svc_indirect(0)
-
-extern void k_rtx_init(void);
-#define rtx_init() _rtx_init((U32)k_rtx_init)
-extern void __SVC_0 _rtx_init(U32 p_func);
-
-extern int k_release_processor(void);
-#define release_processor() _release_processor((U32)k_release_processor)
-extern int __SVC_0 _release_processor(U32 p_func);
-
-extern void *k_request_memory_block(void);
-#define request_memory_block() _request_memory_block((U32)k_request_memory_block)
-extern void *_request_memory_block(U32 p_func) __SVC_0;
-/* __SVC_0 can also be put at the end of the function declaration */
-
-extern int k_release_memory_block(void *);
-#define release_memory_block(p_mem_blk) _release_memory_block((U32)k_release_memory_block, p_mem_blk)
-extern int _release_memory_block(U32 p_func, void *p_mem_blk) __SVC_0;
+void rtx_init(void);
+int release_processor(void);
+void* request_memory_block(void);
+int release_memory_block(void*);
