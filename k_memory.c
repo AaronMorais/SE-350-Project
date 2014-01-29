e6aefa4f394aa1762dca85d86bf5b4cbac288166
Aaron Morais <aaron.m@live.ca>
2014-01-28 21:50:59 -0500

    Make MEM_BLOCK_SIZE more specific

--- k_memory.c
+++ k_memory.c
@@ -54,7 +54,7 @@ we can't know where the heap should go)!
 #endif
 
 #define RAM_END_ADDR 0x10008000
-#define MEM_BLOCK_SIZE 32 // 128 divided by sizeof(MemBlock*)
+#define MEM_BLOCK_SIZE (128 / sizeof(MemBlock*)) // 128 divided by sizeof(MemBlock*)
 
 // The last allocated stack low address. 8 bytes aligned
 // The first stack starts at the RAM high address
