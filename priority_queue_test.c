ebd50bfdefae3b1528b53eceac97bd070aa4852e
crazy2be <code@jmcgirr.com>
2014-01-29 18:33:10 -0500

    Made the priority queue tests pass.

--- priority_queue_test.c
+++ priority_queue_test.c
@@ -3,8 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static PCB* test_queue[PROCESS_PRIORITY_NUM];
+
 static void insert(PCB* pcb) {
-	PriorityStatus status = priority_queue_insert(pcb);
+	PriorityStatus status = priority_queue_insert(test_queue, pcb);
 	if (status != PRIORITY_STATUS_OK) {
 		printf("ERROR, expected status ok, got %d\n", status);
 		exit(1);
@@ -12,12 +14,14 @@ static void insert(PCB* pcb) {
 }
 
 static void pop(PCB* should_be) {
-	PCB* actual = priority_queue_pop();
+	PCB* actual = priority_queue_pop(test_queue);
 	if (actual != should_be) {
 		printf("ERROR, expected PCB at %p, got PCB at %p.\n", should_be, actual);
 		exit(1);
 	}
 }
+
+// TODO: We should test some of the more advanced functions of the priority_queue.
 
 int test_basic() {
 	PCB proc_a = {0};
