ef69458a867f82dbd1b7a26417c4f6001f8b3293
crazy2be <code@jmcgirr.com>
2014-01-29 20:55:49 -0500

    Add comments to heap.c and match naming conventions (thanks Dian :).

--- heap.c
+++ heap.c
@@ -1,26 +1,49 @@
 #include "heap.h"
 
-static unsigned char* free_space_bitmap = NULL;
-static HeapBlock* heap_start = NULL;
-static int num_blocks = 0;
+static U8* s_free_space_bitmap = NULL;
+static HeapBlock* s_heap_start = NULL;
+static int s_num_blocks = 0;
 
 #define BLOCK_FREE 0x00
 #define BLOCK_USED 0xFF
 
+// The heap memory is laid out like this:
+//
+// +---------------------+ start_address
+// | s_free_space_bitmap |
+// +---------------------+  + s_num_blocks
+// |    heap block #1    |
+// + - - - - - - - - - - +  + BLOCK_SIZE
+// |    heap block #2    |
+// + - - - - - - - - - - +  + BLOCK_SIZE
+// |         ...         |
+// + - - - - - - - - - - +  + ...
+// |     heap block n    |
+// +---------------------+ end_address
+//
+// The free space bitmap keeps track of which blocks have been
+// given out, and which blocks are free. The advantage of having
+// this information at the start, as opposed to within the blocks,
+// is that we can scan the entire table very quickly (since it's
+// all contiguous in memory), and there is a reduced possibility
+// of corrupting the heap bookkeeping by memory errors. As compared
+// to a simple free_list implementation, this also allows us to
+// simply and easily gaurd against double frees, frees at invalid
+// addresses, and frees for invalid blocks.
 void heap_init(byte* start_address, byte* end_address) {
-	free_space_bitmap = (unsigned char*)start_address;
-	// +1 for the free_space_bitmap.
-	num_blocks = (end_address - start_address) / (HEAP_BLOCK_SIZE + 1);
+	s_free_space_bitmap = (U8*)start_address;
+	// +1 for the s_free_space_bitmap.
+	s_num_blocks = (end_address - start_address) / (HEAP_BLOCK_SIZE + 1);
 
-	for (int i = 0; i < num_blocks; i++) {
-		free_space_bitmap[i] = BLOCK_FREE;
+	for (int i = 0; i < s_num_blocks; i++) {
+		s_free_space_bitmap[i] = BLOCK_FREE;
 	}
 
-	heap_start = (HeapBlock*)(start_address + num_blocks);
+	s_heap_start = (HeapBlock*)(start_address + s_num_blocks);
 }
 
 HeapStatus heap_free_block(HeapBlock* memory_block) {
-	int offset = memory_block - heap_start;
+	int offset = memory_block - s_heap_start;
 	int is_aligned = (offset % HEAP_BLOCK_SIZE) == 0;
 	if (!is_aligned) {
 		LOG("WARNING: Invalid memory_block passed to heap_free_block.");
@@ -28,23 +51,23 @@ HeapStatus heap_free_block(HeapBlock* memory_block) {
 	}
 
 	int block_number = offset / HEAP_BLOCK_SIZE;
-	if (free_space_bitmap[block_number] != BLOCK_USED) {
+	if (s_free_space_bitmap[block_number] != BLOCK_USED) {
 		LOG("WARNING: Attempted to double-free memory_block!");
 		return HEAP_STATUS_DOUBLE_FREE;
 	}
 
-	if (block_number < 0 || block_number > num_blocks) {
+	if (block_number < 0 || block_number > s_num_blocks) {
 		LOG("WARNING: Attempted to free block outside of valid heap region!");
 		return HEAP_STATUS_INVALID_MEMORY_BLOCK;
 	}
 
-	free_space_bitmap[block_number] = BLOCK_FREE;
+	s_free_space_bitmap[block_number] = BLOCK_FREE;
 	return HEAP_STATUS_OK;
 }
 
 static int heap_find_free_block(void) {
-	for (int i = 0; i < num_blocks; i++) {
-		if (free_space_bitmap[i] == BLOCK_FREE) {
+	for (int i = 0; i < s_num_blocks; i++) {
+		if (s_free_space_bitmap[i] == BLOCK_FREE) {
 			return i;
 		}
 	}
@@ -57,6 +80,6 @@ HeapBlock* heap_alloc_block(void) {
 		LOG("No free memory blocks.");
 		return NULL;
 	}
-	free_space_bitmap[first_free_block] = BLOCK_USED;
-	return &heap_start[first_free_block];
+	s_free_space_bitmap[first_free_block] = BLOCK_USED;
+	return &s_heap_start[first_free_block];
 }
